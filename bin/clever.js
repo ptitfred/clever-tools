8043d739a8820a0bf8d1053079e3551c06cae0f5
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2024-04-19 10:05:12 +0200

    feat(logs): support --app and --org arguments

--- bin/clever.js
+++ bin/clever.js
@@ -797,7 +797,7 @@ function run () {
   const logs = lazyRequirePromiseModule('../src/commands/logs.js');
   const logsCommand = cliparse.command('logs', {
     description: 'Fetch application logs, continuously',
-    options: [opts.alias, opts.before, opts.after, opts.search, opts.deploymentId, opts.addonId, opts.logsFormat],
+    options: [opts.alias, opts.appIdOrName, opts.orgaIdOrName, opts.before, opts.after, opts.search, opts.deploymentId, opts.addonId, opts.logsFormat],
   }, logs('appLogs'));
 
   // MAKE DEFAULT COMMAND
