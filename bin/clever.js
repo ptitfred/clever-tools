d97e051e4112b620635d111ce77cad5d1d529d6a
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2024-04-19 13:45:41 +0200

    feat(service): support --app and --org arguments

--- bin/clever.js
+++ bin/clever.js
@@ -978,7 +978,7 @@ function run () {
   }, service('unlinkAddon'));
   const serviceCommands = cliparse.command('service', {
     description: 'Manage service dependencies',
-    options: [opts.alias, opts.onlyApps, opts.onlyAddons, opts.showAll],
+    options: [opts.alias, opts.appIdOrName, opts.orgaIdOrName, opts.onlyApps, opts.onlyAddons, opts.showAll],
     commands: [serviceLinkAppCommand, serviceUnlinkAppCommand, serviceLinkAddonCommand, serviceUnlinkAddonCommand],
   }, service('list'));
 
