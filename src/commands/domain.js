c0ecf2b4c37585de069e8e12c498d4922bae8a36
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2024-04-23 12:19:53 +0200

    --wip-- [skip ci]

--- src/commands/domain.js
+++ src/commands/domain.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const { Resolver } = require('dns/promises');
+// const dns = require('dns/promises');
+const colors = require('colors/safe');
 const AppConfig = require('../models/app_configuration.js');
 const Logger = require('../logger.js');
 const {
@@ -11,6 +14,12 @@ const {
   removeDomain,
 } = require('@clevercloud/client/cjs/api/v2/application.js');
 const { sendToApi } = require('../models/send-to-api.js');
+const { getSummary } = require('@clevercloud/client/cjs/api/v2/user.js');
+
+const resolver = new Resolver();
+resolver.setServers(['8.8.8.8']);
+
+// const resolver = dns;
 
 function getFavouriteDomain ({ ownerId, appId }) {
   return getFavouriteDomainWithError({ id: ownerId, appId })
@@ -89,4 +98,121 @@ async function rm (params) {
   Logger.println('Your domain has been successfully removed');
 }
 
-module.exports = { list, add, getFavourite, setFavourite, unsetFavourite, rm };
+async function diagApplication (params) {
+
+  // const records = await resolver.resolve('hsablonniere.com', 'A').catch((err) => {
+  //   console.error(err);
+  //   return [];
+  // });
+  //
+  // console.log(records)
+  //
+
+  const { alias } = params.options;
+  const { ownerId, appId } = await AppConfig.getAppDetails({ alias });
+
+  const app = await getApp({ id: ownerId, appId }).then(sendToApi);
+  const expectedDnsForPublicLoadBalancer = await getDefaultLoadBalancersDnsInfo({ id: ownerId, appId }).then(sendToApi);
+
+  const domainMapping = app.vhosts.map((vh) => {
+    const url = new URL('https://' + vh.fqdn);
+    return { hostname: url.hostname, pathPrefix: url.pathname };
+  });
+
+  const { cname: expectedCname, a: expectedA } = expectedDnsForPublicLoadBalancer[0].dns;
+
+  for (const { hostname, pathPrefix } of domainMapping) {
+
+    console.log(colors.blue(hostname) + ' ' + colors.yellow(pathPrefix));
+
+    const realARecords = await resolver.resolve(hostname, 'A').catch((err) => {
+      // console.error(err);
+      return [];
+    });
+    realARecords.length = 0;
+    const realCnameRecords = await resolver.resolve(hostname, 'CNAME')
+      .then((records) => records.map((cname) => cname + '.'))
+      .catch((err) => {
+        // console.error(err);
+        return [];
+      });
+
+    // if cleverapps
+    if (hostname.endsWith('cleverapps.io')) {
+      console.log('  cleverapps.io domains are for test purposes and are automatically configured');
+    }
+
+    // if apex
+    else if (hostname.split('.').length === 2) {
+      for (const aRecord of realARecords) {
+        if (expectedA.includes(aRecord)) {
+          console.log('  A     ' + aRecord.padEnd(15, ' ') + colors.green(' OK'));
+        }
+        else {
+          console.log('  A     ' + aRecord.padEnd(15, ' ') + colors.red(' please remove this record'));
+        }
+      }
+      for (const aRecord of expectedA) {
+        if (!realARecords.includes(aRecord)) {
+          console.log('  A     ' + aRecord.padEnd(15, ' ') + colors.yellow(' please add this record'));
+        }
+      }
+    }
+
+    // if not apex
+    else {
+
+      for (const cnameRecord of realCnameRecords) {
+        if (cnameRecord === expectedCname) {
+          console.log('  CNAME ' + cnameRecord + colors.green(' OK'));
+        }
+        else {
+          console.log('  CNAME ' + cnameRecord + colors.red(' please remove this record'));
+        }
+      }
+      if (!realCnameRecords.includes(expectedCname)) {
+        console.log('  CNAME ' + expectedCname + colors.yellow(' please add this record'));
+      }
+
+      // TODO test A records
+
+      console.log(realARecords.length + ' A records');
+    }
+  }
+
+}
+
+async function diagAll (params) {
+
+  const summary = await getSummary().then(sendToApi);
+  const applications = [
+    ...summary.user.applications.map((app) => [summary.user.id, app.id]),
+    ...summary.organisations.flatMap((o) => o.applications.map((app) => [o.id, app.id])),
+  ];
+
+  for (const [ownerId, appId] of applications) {
+    const app = await getApp({ id: ownerId, appId }).then(sendToApi);
+    console.log(ownerId, appId);
+    const domainMapping = app.vhosts.map((vh) => {
+      const url = new URL('https://' + vh.fqdn);
+      console.log('  ',url.hostname, url.pathname)
+      // return { hostname: url.hostname, pathPrefix: url.pathname };
+    });
+  }
+
+}
+
+// TODO put in clever client
+function getDefaultLoadBalancersDnsInfo (params) {
+  return Promise.resolve({
+    method: 'get',
+    url: `/v4/load-balancers/organisations/${params.id}/applications/${params.appId}/load-balancers/default`,
+    headers: {
+      Accept: 'application/json',
+    },
+    // no query params
+    // no body
+  });
+}
+
+module.exports = { list, add, getFavourite, setFavourite, unsetFavourite, rm, diagApplication, diagAll };
